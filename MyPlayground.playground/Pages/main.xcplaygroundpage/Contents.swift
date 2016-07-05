0916ec96aac3ed824913593b09ac73295fe0768c
Lloyd1nSpace <lloyd1nspace@icloud.com>
2016-07-05 13:18:26 -0400

    Completed the questions

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -15,7 +15,7 @@
 ### 1. Create a variable which represents your bank account balance. (For example: What is a good name for this variable that makes it easily readable for myself now and for the future me _or_ other developers I might be working with? Should I declare it using `let` or `var`? Should it be of type `Int` or `String`?)
 */
 // write your code here
-
+var bankBalance = 0
 
 
 
@@ -24,7 +24,7 @@
 */
 // write your code here
 
-
+let newPuppyName = "Bella"
 
 
 /*: question3
@@ -32,7 +32,7 @@
 */
 // write your code here
 
-
+print(newPuppyName)
 
 
 /*: question4
@@ -40,7 +40,7 @@
 */
 // write your code here
 
-
+print("I just got a new puppy named \(newPuppyName) and she is awesome!")
 
 
 /*: question5
@@ -48,14 +48,15 @@
 */
 // write your code here
 
-
+print ("I have $\(bankBalance) in my bank account.")
 
 
 /*: question6
 ### 6. Congratulations! You just got $100 for your birthday, so now you have $100 more in your bank account. Update your bank account with the new balance and print "I now have $<balance>." to the console.
 */
 // write your code here
-
+bankBalance += 10
+print("I now have $\(bankBalance).")
 
 
 
@@ -63,7 +64,8 @@
 ### 7. You decided you don't like the name Bella. Change your puppy's name to something else. (Can you do this? What happens when you try? Why?)
 */
 // write your code here
-
+newPuppyName = "Reina"
+// can't change newPuppyName because it is a constant (let) meaning it cannot be altered!
 
 
 
